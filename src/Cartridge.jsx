91e96ba3793e339b49f9786a1385ca3d460a1e59
brunosimon <simon.bruno.77@gmail.com>
2023-07-31 16:53:06 +0100

    :lipstick: Improve render + :bento: Cartridge > Improve

--- src/Cartridge.jsx
+++ src/Cartridge.jsx
@@ -9,7 +9,7 @@ export function Cartridge()
     const config = useControls(
         'cartridge.material',
         {
-            transmissionSampler: true,
+            transmissionSampler: false,
             backside: true,
             samples: { value: 10, min: 1, max: 32, step: 1 },
             resolution: { value: 2048, min: 256, max: 2048, step: 256 },
@@ -22,18 +22,18 @@ export function Cartridge()
             distortion: { value: 0.0, min: 0, max: 1, step: 0.01 },
             distortionScale: { value: 0.3, min: 0.01, max: 1, step: 0.01 },
             temporalDistortion: { value: 0.5, min: 0, max: 1, step: 0.01 },
-            clearcoat: { value: 1, min: 0, max: 1 },
+            clearcoat: { value: 0, min: 0, max: 1 },
             attenuationDistance: { value: 5, min: 0, max: 10, step: 0.01 },
             attenuationColor: '#212121',
             color: '#ffffff',
-            bg: '#ffffff'
+            bg: '#000000'
         }
     )
 
     return <>
         <mesh
             rotation-x={ Math.PI * 0.5 }
-            geometry={ nodes.cartridge004.geometry }
+            geometry={ nodes.cartridge003.geometry }
             // material={ materials.cartridge }
         >
             <MeshTransmissionMaterial
--- src/Stage.jsx
+++ src/Stage.jsx
@@ -6,13 +6,14 @@ export function Stage()
     const environmentConfig = useControls(
         'stage.environment',
         {
-            preset: { value: 'apartment', options: [ 'apartment', 'city', 'dawn', 'forest', 'lobby', 'night', 'park', 'studio', 'sunset', 'warehouse', ] },
-            background: { value: true },
+            preset: { value: 'studio', options: [ 'apartment', 'city', 'dawn', 'forest', 'lobby', 'night', 'park', 'studio', 'sunset', 'warehouse', ] },
+            background: { value: false },
             blur: { value: 0.5, min: 0, max: 1, step: 0.001 },
         }
     )
     
     return <>
+        <color attach={ 'background' } args={ [ '#111111' ] } />
         <Environment { ...environmentConfig } />
     </>
 }
